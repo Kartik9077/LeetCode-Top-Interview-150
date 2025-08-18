bd08acda3afe88597740e81e2aa101fa669c3560
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-08-18 16:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (97.33%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+ int square(int num){
+    int ans=0;
+    while(num>0){
+        int rem=num%10;
+        ans+=rem*rem;
+        num/=10;
+    }
+  return ans;
+ }
+    bool isHappy(int n) {
+        int fast=n;
+        int slow=n;
+        do{
+            slow=square(slow);
+            fast=square(square(fast));
+
+            
+        }while(slow!=fast);
+        return slow==1;
+        
+        
+    }
+};
