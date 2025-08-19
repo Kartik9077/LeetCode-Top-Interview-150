411c4cd8947a51f3c20847bed1ed86776a5907f4
Kartik Sharma <kartiksharma15032005@gmail.com>
2025-08-19 10:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 111.4 MB (40.61%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -5,9 +5,8 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0)count++;
             else {
-                if(count!=0){
-                    ans+=count*(count+1)/2;
-                }
+              
+               ans+=count*(count+1)/2;
                 count=0;
             }
         }
